89bfcee3cd0fac19c3beb374833ced7a387e78d9
Brendon Ward <brendon.c.ward@gmail.com>
2019-08-11 11:35:23 +1000

    Create front-end components

--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -1,39 +1,19 @@
 import React from 'react';
 
-import Post from './Post';
 import samplePosts from '../sample-posts'
-import AddPost from './addPost';
+import ShortenURL from './shortenURL';
 
 class Home extends React.Component {  
     state = {
         posts: samplePosts
     }
 
-    addPost = post => {
-        // 1. Take a copy of the existing state
-        const posts = { ...this.state.posts };
-        // 2. Add our new fish to that fishes variable
-        posts[`post${Date.now()}`] = post;
-        // 3. Set the new fishes object to state
-        this.setState({ posts });
-    };
-
     render() {
         console.log(this.state.posts)
         return (
             <div className="home">
-                <h1>Home</h1>
-                <div className="photo-grid">
-                    {Object.keys(this.state.posts).map(key => (
-                        <Post
-                            key={key}
-                            index={key}
-                            details={this.state.posts[key]}
-                            {...this.props}
-                        />
-                    ))}
-                </div>
-                <AddPost addPost={this.addPost}/>
+                <h1>Shorten a URL</h1>
+                <ShortenURL/>
             </div>
         )
     }
---
+++ Frontend/src/components/shortenURL.js
@@ -0,0 +1,36 @@
+import React from "react";
+import axios from "axios";
+
+class AddPost extends React.Component {
+  urlRef = React.createRef();
+
+  shortenURL = async event => {
+    // 1.  stop the form from submitting
+    event.preventDefault();
+
+    const response = await axios.post(
+      'http://61a2a231.ngrok.io/api/url/shorten',
+      //Body
+      {"longUrl": "https://www.amazon.com.au/FITBIT-Versa-Peach-Rose-Gold/dp/B07B9WFFJV?pf_rd_p=6cf8387c-7649-4d6e-a715-18f9cf456319&pd_rd_wg=v6eqI&pf_rd_r=9BXGEB8E5EH8RM451PYG&ref_=pd_gw_unk&pd_rd_w=fiBtR&pd_rd_r=d50d8b16-8f6f-47d0-aa32-a7cab4f016dc"},
+      //Headers
+      { headers: { 'Content-Type': 'application/json' } }
+    )
+    console.log(response.data)
+
+    // const url = {
+    //   url: this.urlRef.current.value,
+    // };
+    // refresh the form
+    event.currentTarget.reset();
+  };
+  render() {
+    return (
+      <form className="url-shortener" onSubmit={this.shortenURL}>
+        <input name="long-url" ref={this.urlRef} type="text" placeholder="URL to be shortened..." />
+        <button type="submit">+ Shorten URL</button>
+      </form>
+    );
+  }
+}
+
+export default AddPost;
